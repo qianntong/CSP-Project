24971c594e83e10e824dc698c33e438bd6801641
kylebathgate <kyle.bathgate@gmail.com>
2024-11-19 15:10:49 -0600

    Variable yard dimensions

--- DP Basic.py
+++ DP Basic.py
@@ -1,17 +1,27 @@
 import numpy as np
 import random
 
-n = 8 # set number of incoming containers to be stacked (must be less than 9 for this case)
+n = 30 # set number of incoming containers to be stacked (must be less than 9 for this case)
+
+H = 5 # max allowable stack height
+B = 6 # number of container columns
 
 C = np.arange(1,n+1) # generate 6 containers
 random.shuffle(C) # randomly assign priorities by shuffling
+
+#C= np.array([1, 2, 3, 8, 7, 4, 6, 5]) # bad case for 3 x 3
 print(f"Incoming container priority order: {C} \n")
 Clen = len(C)
 
-State = np.zeros((Clen+1, 3, 3))
-Actions = np.array([[2, 0],
-                    [2, 1],
-                    [2, 2]]) #first entry is height, second entry is stack
+State = np.zeros((Clen+1, H, B))
+
+# define actions
+Actions = np.array([H-1,0])
+for b in range(1,B):
+    Actions= np.vstack((Actions, np.array([H-1, b])))
+print(Actions)
+
+
 
 def bubble_sort(cycle):
     Ylen, Ycol = cycle.shape
@@ -26,7 +36,7 @@ def bubble_sort(cycle):
     return swaps
 
 def stack(state, a, t):
-    newcycle = np.zeros((3, 3))
+    newcycle = np.zeros((H, B))
     newcycle[a[0]][a[1]] = C[t]
     cycle = state[t] + newcycle
     cyclecopy = cycle.copy()
@@ -40,7 +50,7 @@ for t in range(0,Clen):
         V_q = stack(State, q, t)
         V_c = np.append(V_c, V_q[0])
         V_r = np.append(V_r, V_q[1]) 
-    V_c = np.reshape(V_c, (len(Actions),3,3))                     
+    V_c = np.reshape(V_c, (len(Actions),H,B))                     
     A = int(np.argmin(V_r))
     print(f"Container {C[t]} will be placed in column {A}")
     print(f'Total number of reshuffles:{int(np.min(V_r))} \n')
@@ -56,10 +66,9 @@ print(State)
 
 
 print(f"\nTotal number of reshuffles to clear stack: {int(min(V_r))} \n")
-print(f"Final yard configuration:\n{State[-1]}")
+print(f"Final yard configuration:\n{State[-1]}\n")
     
-    
+print(f"Incoming container priority order: {C}")
 
 # bad case: [1 2 3 8 7 4 6 5] 
-        
         
